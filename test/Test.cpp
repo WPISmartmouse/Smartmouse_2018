29a6627ccaedc208530b71da09c93905537f73b7
Peter_Mitrano <mitranopeter@gmail.com>
2016-02-08 12:15:33 -0500

    added in bounds test

--- test/Test.cpp
+++ test/Test.cpp
@@ -2,38 +2,21 @@
 #include "Node.h"
 #include "gtest/gtest.h"
 
-class FooTest : public ::testing::Test {
- protected:
-
-  FooTest() {
-    // You can do set-up work for each test here.
-  }
-
-  virtual ~FooTest() {
-    // You can do clean-up work that doesn't throw exceptions here.
-  }
-
-  // If the constructor and destructor are not enough for setting up
-  // and cleaning up each test, you can define the following methods:
-
-  virtual void SetUp() {
-    // Code here will be called immediately after the constructor (right
-    // before each test).
-  }
-
-  virtual void TearDown() {
-    // Code here will be called immediately after each test (right
-    // before the destructor).
-  }
-
-  // Objects declared here can be used by all tests in the test case for Foo.
-};
-
-TEST(NodeTest, OutOfBounds) {
+TEST(NodeTest, OutOfBoundsGetNode) {
   AbstractMaze maze;
   Node *n;
   int status = maze.get_node(&n, -1, -1);
   EXPECT_EQ(status, Node::OUT_OF_BOUNDS);
+
+  status = maze.get_node(&n, AbstractMaze::MAZE_SIZE, AbstractMaze::MAZE_SIZE);
+  EXPECT_EQ(status, Node::OUT_OF_BOUNDS);
+}
+
+TEST(NodeTest, InBoundsGetNode) {
+  AbstractMaze maze;
+  Node *n;
+  int status = maze.get_node(&n, 0, 0);
+  EXPECT_EQ(status, 0);
 }
 
 int main(int argc, char **argv) {
