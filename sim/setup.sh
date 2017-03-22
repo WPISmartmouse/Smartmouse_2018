c8b79c00b0f64be8f79dc25824942e350455c29c
Peter Mitrano <mitranopeter@gmail.com>
2017-03-22 03:23:29 -0400

    reverted mouse plugin
    still no good setup for switching between src and non-source gazebo
    installs

--- sim/setup.sh
+++ sim/setup.sh
@@ -1,7 +1,8 @@
 # MUST BE RUN FROM root of the project (Smartmouse_2017)
-source /usr/local/share/gazebo/setup.sh
+source /usr/share/gazebo/setup.sh
+#source /usr/local/share/gazebo/setup.sh
 export GAZEBO_PLUGIN_PATH=${PWD}/.build/sim:$GAZEBO_PLUGIN_PATH
-export LD_LIBRARY_PATH=${PWD}/.build/sim:/usr/local/lib/$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=${PWD}/.build/sim: #/usr/local/lib/$LD_LIBRARY_PATH
 export GAZEBO_MODEL_PATH=${PWD}/sim/models
 
 # gazebo -u --seed 0 --verbose sim/gzmaze.world
