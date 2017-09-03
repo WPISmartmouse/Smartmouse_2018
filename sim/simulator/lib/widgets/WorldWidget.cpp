0d1d6cca0b22def341bdc159787058aa7ed56e1a
Peter Mitrano <mitranopeter@gmail.com>
2017-09-03 18:13:59 -0400

    QTransform so drawing is in meters w/ nice origin

--- sim/simulator/lib/widgets/WorldWidget.cpp
+++ sim/simulator/lib/widgets/WorldWidget.cpp
@@ -9,30 +9,28 @@ WorldWidget::WorldWidget() {
 
 void WorldWidget::paintEvent(QPaintEvent *event) {
   QPainter painter(this);
+  QTransform tf;
+  {
 
-  QRect r = this->geometry();
+    QRect r = this->geometry();
 
-  int w = std::min(r.width(), r.height()) - 1;
-  double m2p = w / AbstractMaze::MAZE_SIZE_M;
+    int w = std::min(r.width(), r.height()) - 1;
+    double m2p = w / AbstractMaze::MAZE_SIZE_M;
 
-  int cx = (r.width() - w) / 2;
-  int cy = (r.height() - w) / 2;
+    int cx = (r.width() - w) / 2;
+    int cy = (r.height() - w) / 2;
+    tf.translate(cx, cy);
+    tf = QTransform::fromScale(m2p, m2p);
+  }
 
-  painter.translate(cx, cy);
-
-  QRectF base = QRectF(0, 0, AbstractMaze::MAZE_SIZE_M * m2p, AbstractMaze::MAZE_SIZE_M * m2p);
-  painter.drawRect(base);
+  QRectF base = QRectF(0, 0, AbstractMaze::MAZE_SIZE_M, AbstractMaze::MAZE_SIZE_M);
+  painter.drawRect(tf.mapRect(base));
 
   for (unsigned int i = 0; i < AbstractMaze::MAZE_SIZE; i++) {
-    QLineF h_line(0,
-                  m2p * (i * AbstractMaze::UNIT_DIST),
-                  m2p * AbstractMaze::MAZE_SIZE_M,
-                  m2p * (i * AbstractMaze::UNIT_DIST));
-    QLineF v_line(m2p * (i * AbstractMaze::UNIT_DIST),
-                  0,
-                  m2p * (i * AbstractMaze::UNIT_DIST),
-                  m2p * AbstractMaze::MAZE_SIZE_M);
-    painter.drawLine(h_line);
-    painter.drawLine(v_line);
+    QLineF h_line(0, i * AbstractMaze::UNIT_DIST, AbstractMaze::MAZE_SIZE_M, i * AbstractMaze::UNIT_DIST);
+    painter.drawLine(tf.map(h_line));
+
+    QLineF v_line((i * AbstractMaze::UNIT_DIST), 0, (i * AbstractMaze::UNIT_DIST), AbstractMaze::MAZE_SIZE_M);
+    painter.drawLine(tf.map(v_line));
   }
 }
