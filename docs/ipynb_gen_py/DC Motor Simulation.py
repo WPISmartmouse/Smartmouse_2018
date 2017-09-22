61e0465bd760a494142ff7ac28eed5687bce0dc6
Peter Mitrano <mitranopeter@gmail.com>
2017-09-22 00:20:44 -0400

    more proof of a good DC motor model

--- docs/ipynb_gen_py/DC Motor Simulation.py
+++ docs/ipynb_gen_py/DC Motor Simulation.py
@@ -11,41 +11,60 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-# In[33]:
+# In[49]:
 
-theta_dot = 0
-i = 0
+def simulate(V, J, b, K, R, L, theta_dot=0, i=0):
+    # simulate T seconds
+    T = 5
+    dt = 0.01
+    theta_dots = []
+    ts = np.arange(0, T, dt)
+    for t in ts:
+        A = np.array([[-b/J,K/J],[-K/L,-R/L]])
+        B = np.array([0, 1/L])
+        x = np.array([theta_dot, i])
+        w = V(t, i, theta_dot)
+        x_dot = A@x+B*w
+        theta_dot += (x_dot[0] * dt)
+        i += (x_dot[1] * dt)
+        theta_dots.append(theta_dot)
+    
+    return ts, theta_dots
 
-# constants
-J = 0.01
-b = 0.1
-K = 0.01
-R = 1
-L = 0.5
+def const_V(v):
+    def _v(t, i, theta_dot):
+        return v
+    return _v
 
-# input
-def V(t):
-    return 1
-
-# simulate T seconds
-T = 5
-dt = 0.01
-theta_dots = []
-ts = np.arange(0, T, dt)
-for t in ts:
-    A = np.array([[-b/J,K/J],[-K/L,-R/L]])
-    B = np.array([0, 1/L])
-    x = np.array([theta_dot, i])
-    w = V(t)
-    x_dot = A@x+B*w
-    theta_dot += (x_dot[0] * dt)
-    i += (x_dot[1] * dt)
-    theta_dots.append(theta_dot)
-#     print(A, x, B, w, "->", x_dot)
-
+ts, theta_dots = simulate(const_V(1), J=0.01, b=0.1, K=0.01, R=1, L=0.5)
+plt.figure()
 plt.plot(ts, theta_dots)
+plt.title("Motor response to 1v input")
 plt.ylabel("Speed (rad/s)")
 plt.xlabel("Time (seconds)")
+
+ts, theta_dots = simulate(const_V(5), J=0.01, b=0.1, K=0.01, R=1, L=0.5)
+plt.figure()
+plt.plot(ts, theta_dots)
+plt.title("Motor response to 5v input")
+plt.ylabel("Speed (rad/s)")
+plt.xlabel("Time (seconds)")
+
+ts, theta_dots = simulate(const_V(5), J=0.05, b=0.1, K=0.01, R=1, L=0.5)
+plt.figure()
+plt.plot(ts, theta_dots)
+plt.title("Heavier motor response to 5v input")
+plt.ylabel("Speed (rad/s)")
+plt.xlabel("Time (seconds)")
+
+ts, theta_dots = simulate(const_V(5), J=0.05, b=0.1, K=0.01, R=1, L=0.5, theta_dot=1)
+plt.figure()
+plt.plot(ts, theta_dots)
+plt.title("Already spinning motor response to 5v input")
+plt.ylabel("Speed (rad/s)")
+plt.xlabel("Time (seconds)")
+
+
 plt.show()
 
 
