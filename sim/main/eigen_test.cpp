fdbf53d955c60c848810d5c15f31b5506805d96f
Peter Mitrano <mitranopeter@gmail.com>
2017-09-28 13:20:38 -0400

    simple eigen test
    haven't tested on arm

---
+++ sim/main/eigen_test.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <Eigen/Dense>
+
+int main()
+{
+  Eigen::Matrix<double, 3, 2> A;
+  A << 0.68,0.597, -0.211,0.823, 0.566,-0.605;
+  std::cout << "Here is the matrix A:\n" << A << std::endl;
+  Eigen::Matrix<double, 3, 1> b;
+  b << -0.33, 0.536, -0.444;
+  std::cout << "Here is the right hand side b:\n" << b << std::endl;
+  auto sln = A.jacobiSvd(Eigen::ComputeFullU | Eigen::ComputeFullV).solve(b);
+  std::cout << "The least-squares solution is:\n" << sln << std::endl;
+}
