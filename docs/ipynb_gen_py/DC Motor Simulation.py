c048cf15d857c1642cb2050c3cb4dd45c244aefc
Peter Mitrano <mitranopeter@gmail.com>
2017-10-02 10:34:21 -0400

    apparently our motor model is linear
    which is unfortunate

--- docs/ipynb_gen_py/DC Motor Simulation.py
+++ docs/ipynb_gen_py/DC Motor Simulation.py
@@ -5,13 +5,13 @@
 
 # Look at the dynamics_model.pdf for the explanation behind this code.
 
-# In[1]:
+# In[2]:
 
 import numpy as np
 import matplotlib.pyplot as plt
 
 
-# In[21]:
+# In[3]:
 
 def simulate(V, J, b, K, R, L, theta_dot=0, i=0):
     # simulate T seconds
@@ -70,23 +70,38 @@ plt.xlabel("Time (seconds)")
 plt.show()
 
 
-# In[90]:
+# In[4]:
 
 ts, theta_dots, currents = simulate(const_V(5), J=0.00005, b=0.0004, K=0.01, R=2.5, L=0.1)
-# ts, theta_dots, currents = simulate(const_V(5), J=0.01, b=0.1, K=2, R=1, L=0.5)
 
 plt.figure(figsize=(11,5))
 plt.subplot(121)
 plt.plot(ts, theta_dots * 0.0145)
-plt.title("Smartmouse Motor at max voltage")
+plt.title("Speed of realistic motor")
 plt.ylabel("Speed (meter/s)")
 plt.xlabel("Time (seconds)")
 
 plt.subplot(122)
 plt.plot(ts, currents)
-plt.title("Smartmouse Motor at max voltage")
+plt.title("Current of realistic motor")
 plt.ylabel("Current (amps)")
 plt.xlabel("Time (seconds)")
+plt.show()
+
+
+# In[27]:
+
+speeds = []
+voltages = np.linspace(0, 5, 255)
+for v in voltages:
+    def vfunc(*args):
+        return v
+    speed_mps = simulate(vfunc, J=0.00005, b=0.0004, K=0.01, R=2.5, L=0.1)[1][-1] * 0.0145
+    speeds.append(speed_mps)
+
+plt.plot(speeds, voltages)
+plt.xlabel("Voltage")
+plt.ylabel("Steady State Speed (m/s)")
 plt.show()
 
 
