63605b2d56aff474a5973fdb5756ca67ce682abf
Peter Mitrano <mitranopeter@gmail.com>
2017-11-01 10:13:59 -0400

    fix tests to match new coordinate system

--- common/test/CommonTest.cpp
+++ common/test/CommonTest.cpp
@@ -15,9 +15,9 @@ TEST(ForwardKinematicsTest, Forward_tenth_second) {
   double drow, dcol, dtheta;
   std::tie(dcol, drow, dtheta) = KinematicController::forwardKinematics(1, 1, 0, 0.1);
 
-  EXPECT_EQ(dcol, 0.1);
-  EXPECT_EQ(drow, 0);
-  EXPECT_EQ(dtheta, 0);
+  EXPECT_DOUBLE_EQ(dcol, 0.1);
+  EXPECT_DOUBLE_EQ(drow, 0);
+  EXPECT_DOUBLE_EQ(dtheta, 0);
 }
 
 TEST(ForwardKinematicsTest, half_cw_turn_one_second) {
@@ -25,8 +25,8 @@ TEST(ForwardKinematicsTest, half_cw_turn_one_second) {
   std::tie(dcol, drow, dtheta) = KinematicController::forwardKinematics(smartmouse::kc::TRACK_WIDTH * M_PI, 0, 0, 1);
 
   EXPECT_NEAR(dcol, 0, 1e-9);
-  EXPECT_EQ(drow, smartmouse::kc::TRACK_WIDTH);
-  EXPECT_EQ(dtheta, -M_PI);
+  EXPECT_DOUBLE_EQ(drow, smartmouse::kc::TRACK_WIDTH);
+  EXPECT_DOUBLE_EQ(dtheta, M_PI);
 }
 
 TEST(ForwardKinematicsTest, half_ccw_turn_one_second) {
@@ -34,8 +34,8 @@ TEST(ForwardKinematicsTest, half_ccw_turn_one_second) {
   std::tie(dcol, drow, dtheta) = KinematicController::forwardKinematics(0, smartmouse::kc::TRACK_WIDTH * M_PI, 0, 1);
 
   EXPECT_NEAR(dcol, 0, 1e-9);
-  EXPECT_EQ(drow, -smartmouse::kc::TRACK_WIDTH);
-  EXPECT_EQ(dtheta, M_PI);
+  EXPECT_DOUBLE_EQ(drow, -smartmouse::kc::TRACK_WIDTH);
+  EXPECT_DOUBLE_EQ(dtheta, -M_PI);
 }
 
 TEST(ForwardKinematicsTest, cw_turn_one_second) {
@@ -44,7 +44,7 @@ TEST(ForwardKinematicsTest, cw_turn_one_second) {
 
   EXPECT_NEAR(dcol, 0, 1e-9);
   EXPECT_NEAR(drow, 0, 1e-9);
-  EXPECT_EQ(dtheta, -2 * M_PI);
+  EXPECT_DOUBLE_EQ(dtheta, 2 * M_PI);
 }
 
 TEST(ForwardKinematicsTest, ccw_turn_one_second) {
@@ -53,7 +53,7 @@ TEST(ForwardKinematicsTest, ccw_turn_one_second) {
 
   EXPECT_NEAR(dcol, 0, 1e-9);
   EXPECT_NEAR(drow, 0, 1e-9);
-  EXPECT_EQ(dtheta, 2 * M_PI);
+  EXPECT_DOUBLE_EQ(dtheta, -2 * M_PI);
 }
 
 int main(int argc, char **argv) {
