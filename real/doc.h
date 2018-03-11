ddaa79b9344342ccd0b14e10ae81372c750b76b4
Peter Mitrano <mitranopeter@gmail.com>
2018-03-11 10:25:55 -0400

    regenerate documentation
     - remove latex generation

--- real/doc.h
+++ real/doc.h
@@ -1,36 +1,19 @@
-/** \mainpage SmartMouse V4 Index Page
+/** \mainpage SmartMouse 2018 Index Page
  *
  * \section intro_sec Introduction
  *
  * All function documentation is done in the header files. To see notes for each function, click on the header file it belongs to.
  *
  * If there are any issues/bugs, use the issue tracker on the github repository.
- * 	[Issue tracker] (https://github.com/WPISmartmouse/SmartMouse2015_Maze/issues)
+ * 	[Issue tracker] (https://github.com/WPISmartmouse/SmartMouse_2018/issues)
  *
  * \section run_sec_linux Building & Running (Linux)
  *
  * to make, run
  *
- *      make;./bin/explore
- *
- * you can make more debug information print out by making with the ARGS argument
- *
- *      make ARGS=-DDEBUG_PATH #this will show the mouse in the maze as it goes. press enter to step forward
- *      make ARGS=-DDEBUG #minimal debug info
- *      make ARGS=-DDBEUG_FULL #a whole ton of debug information
- *
- * \section install_sec_linux Installation (Linux)
- *
- * \subsection step1_linux Clone the repository
- *
- *      git clone https://github.com/WPISmartmouse/SmartMouse2015_Maze/
- *
- * \section install_sec_mac Installation (Mac)
- *
- * \subsection step1_mac Figure it out yourself
- *
- * \section install_sec_win Installation (Windows)
- *
- * \subsection step1_win No.
+ *      mkdir .real_build
+ *      cd .real_build
+ *      cmake ..
+ *      make main_Upload
  *
  */
