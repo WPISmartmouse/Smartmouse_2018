ab53d0449466a5d7d53c3af90320b70b612c3abc
Peter Mitrano <mitranopeter@gmail.com>
2018-03-31 18:52:23 -0400

    models from one sample are very accurate (obviously)
    - need to try again with another dataset
    - need to see how accurate an "average" model would be

--- docs/ir_sensor_testing/plot.py
+++ docs/ir_sensor_testing/plot.py
@@ -3,12 +3,17 @@
 import argparse
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy import optimize
 
 
 def main():
+    np.warnings.simplefilter("ignore", optimize.OptimizeWarning)
+    np.set_printoptions(suppress=True, precision=6)
+
     parser = argparse.ArgumentParser("plot a log file from analog_test.cpp")
     parser.add_argument("--logs", help="csv file output form logging serial", nargs="*", required=True)
     parser.add_argument("--columns", help="number of column to plot (0 indexed)", nargs="*", type=int, required=True)
+    parser.add_argument("--no-plot", help="skip plotting", action="store_true")
     parser.add_argument("--samples-per-interval", '-n', help="number of readings at each distance", type=int, default=10)
     parser.add_argument("--number-of-distances", '-d', help="number of distances", type=int, default=17)
     parser.add_argument("--spacing", '-s', help="spacing between each interval in meters", type=float, default=0.005)
@@ -24,26 +29,56 @@ def main():
     N = args.samples_per_interval
     D = args.number_of_distances
     distances = np.arange(D, 0, -1) * args.spacing
+    distances[0] = 0.2
+
     data = np.ndarray((len(args.logs), D, N))
     for i, (column, log) in enumerate(zip(args.columns, args.logs)):
         x = np.genfromtxt(log, delimiter=', ')[:, column]
         x = x.reshape((D, N))
         data[i] = x
 
+    # compute the mean and errors for each interval
     means = data.mean(axis=2)
-    errors = np.stack((data.max(axis=2) - means, means - data.min(axis=2)), axis=1)
+    ranges = np.stack((data.max(axis=2) - means, means - data.min(axis=2)), axis=1)
 
-    # compute the mean for each interval
+    # fit our model Y=a*X^b+c for each sensor
+    params = np.ndarray((S, 3))
+    model_errors = np.ndarray((S, D-4))
+    print("|sensor|a|b|c|stddev|")
+    print("|------|-|-|-|------|")
+    for i, m in enumerate(means):
+        def model(x, a, b, c):
+            return a*pow(x, b) + c
+        # we ignore the first data point here because it doesn't have a proper distance, it's infinitly far
+        # we also ignore the last three points where shit starts to go down
+        m = m[1:-3]
+        d = distances[1:-3]
+        p, _ = optimize.curve_fit(model, m, d, maxfev=10000)
+        model_error = model(m, *p) - d
+        print("|{:s}|{:0.6f}|{:0.6f}|{:0.6f}|".format(args.logs[i].strip(".csv"), *p))
+        params[i] = p
+        model_errors[i] = model_error
 
-    plt.figure()
-    for d, yerr, log in zip(data, errors, args.logs):
-        plt.scatter(distances, d.mean(axis=1), label=log, s=4)
-        plt.errorbar(x=distances, y=d.mean(axis=1), yerr=yerr, linewidth=1)
+    if not args.no_plot:
+        plt.figure()
+        plt.plot([distances[1], distances[-3]], [0, 0], label='zero', linestyle='--')
+        for model_error, log in zip(model_errors, args.logs):
+            plt.plot(distances[1:-3], model_error, label=log)
+        plt.title("Modeling Error")
+        plt.xlabel("distance")
+        plt.ylabel("error (meters)")
+        plt.legend()
 
-    plt.legend()
-    plt.xlabel("sample")
-    plt.ylabel("analog read value")
-    plt.show()
+        plt.figure()
+        for d, xerr, log in zip(data, ranges, args.logs):
+            plt.scatter(x=d.mean(axis=1), y=distances, label=log, s=4)
+            plt.errorbar(x=d.mean(axis=1), y=distances, xerr=xerr, linewidth=1)
+        plt.title("Averaged Data")
+        plt.xlabel("ADC Value 0-8192")
+        plt.ylabel("Distance (meters)")
+        plt.legend()
+
+        plt.show()
 
 
 if __name__ == '__main__':
